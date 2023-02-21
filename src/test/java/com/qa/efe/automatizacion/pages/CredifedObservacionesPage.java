b4c19e98f4f47ca7f5ac611d4e004ec6ce29ad86
joseniquen <josecarlosniquenalvarado@gmail.coom>
2023-02-21 12:53:01 -0500

    s
    sss

--- src/test/java/com/qa/efe/automatizacion/pages/CredifedObservacionesPage.java
+++ src/test/java/com/qa/efe/automatizacion/pages/CredifedObservacionesPage.java
@@ -4,12 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
 
 import com.qa.efe.automatizacion.shared.SeleniumWaiters;
 import com.qa.efe.automatizacion.shared.devices.managers.WebDriverManager;
